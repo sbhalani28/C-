9a32168036479161316d774be875bc0d5013c4d7
shivani bhalani <144037713+sbhalani28@users.noreply.github.com>
2023-11-03 19:41:42 +0530

    Add files via upload

---
+++ min-max value(form usr).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter A:-";
+    cin>>a;
+    cout<<"enter B:-";
+    cin>>b;
+    if(a>b){cout<<"A is max";}
+    else{
+        cout<<"B is big";
+        return 0;
+    }
+}
---
+++ sum(from usr).cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter no A:-";
+    cin>>a;
+    cout<<"Enter no B:-";
+    cin>>b;
+    c=a+b;
+    cout<<"ans="<<c;
+    return 0;
+    
+}
