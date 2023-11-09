6dc035dccf137a5b54f13a2259e25c849de0c569
shivani bhalani <144037713+sbhalani28@users.noreply.github.com>
2023-11-09 10:59:42 +0530

    Add files via upload

---
+++ class & object(normal program).cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class Demo
+{
+    Public:
+    void data()
+    {
+        cout<<"hello world";
+    }
+
+};
+int main()
+{
+    Demo d;
+    d.data();
+}
---
+++ class & object[sum(udf)].cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Ex
+{
+    Public:
+    void data(int a,int b)
+    {
+        cout<<"sum="<<a+b;
+
+    }
+};
+int main()
+{
+    Ex d;
+    d.data(10,5);
+    
+}
